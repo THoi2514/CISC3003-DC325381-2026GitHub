d09fa4fcd8748040963491b3052571ff23055318
Thomas Hoi <dc32538@um.edu.mo>
2026-04-03 20:36:02 +0800

    Initial commit

---
+++ CISC3003-Practice10-2026APR03/public/cisc3003-practice10.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>dc325381 Thomas,Hoi Kai Cheng PHP</title>
+</head>
+<body>
+    <h1>Thomas,Hoi Kai Cheng PHP</h1>
+    
+    <p>The ASCII art of the first letter of my name:</p>
+<pre>
+    TTTTTTTTTTT
+         T
+         T
+         T
+         T
+         T
+</pre>
+
+    <p>The SHA256 hash of "Thomas" is:</p>
+    <?php
+        print hash('sha256', 'Thomas');
+    ?>
+</body>
+</html>
